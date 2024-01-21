b048c34e052a150ee326524f9b7c64d7ebf39ab1
Mike-Slim <2085867730@qq.com>
2024-01-21 23:22:07 +0800

    4

---
+++ c/24.01.21.01.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int a,b;
+    a=9;
+    b=8;
+    if (a>=b){
+        printf("%d\n",a);
+
+    }
+    else{
+        printf("%d\n",b);
+    }
+    
+
+
+
+    return 0;}
---
+++ c/24.01.21.02.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+int main() {
+    int a,x;
+    x=(a=3*5,a+2),a+2;
+    printf("a=%d\n",a);
+    printf("x=%d\n",x);
+    //printf("a=%d\n",a+2);
+
+
+
+    return 0;}
